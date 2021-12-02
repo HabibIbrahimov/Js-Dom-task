6f0b9bc6f48d91c984fa2300b56d030b03677b4a
HabibIbrahimov <habibyi@code.edu.az>
2021-12-02 23:36:19 +0400

    finished

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,8 @@
 let btnAdd=document.getElementById("btnAdd");
 let input=document.getElementById("input");
 let list=document.getElementById("list");
+let btnRemove=document.getElementById("btnRemove");
+let fas=document.getElementById("fas");
 
 btnAdd.addEventListener("click",function(){
     let value=input.value.trim();
@@ -8,12 +10,26 @@ btnAdd.addEventListener("click",function(){
         alert("Do not leave empty");
         return;
     };
-    let li=document.createElement("li") ;
-    let li=document.createElement("i");
-    
+    let li=document.createElement("li");
+    let i=document.createElement("i");
+    i.onclick=function(){
+        li.remove();
+    }
+    i.classList.add("fas");
+    i.classList.add("fa-trash-alt");
+    i.setAttribute("id","fas");
+
     li.classList.add("list-group-item");
     li.classList.add("test");
     li.innerText=value;
     list.appendChild(li);
-    input.value="";    
-})
+    li.appendChild(i);
+    input.value=""; 
+
+});
+
+btnRemove.addEventListener("click",function(){
+   
+        list.innerHTML="";
+        list.firstChild.remove();
+    });
